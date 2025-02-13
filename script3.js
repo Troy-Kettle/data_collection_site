7ced6f77056c470c0131c76dd203ff069ece9b17
Troy-Kettle <troykettle19@gmail.com>
2025-02-13 11:04:48 +0000

    Updates to homepage and part3 scenarios

--- script3.js
+++ script3.js
@@ -45,148 +45,154 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
 
-    const scenarios = [
-       {
-           id: 1,
-           data: {
-               "Heart rate (beats/min)": [112, 105, 113, 109, 110],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 2,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 85, 92, 110],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 3,
-           data: {
-               "Heart rate (beats/min)": [131, 126, 123, 115, 110],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 4,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["97/63", "93/66", "99/57", "96/60", "95/55"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 5,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "128/71", "103/60", "95/55"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 6,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["79/49", "83/54", "89/60", "92/57", "95/55"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 7,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [24, 23, 23, 25, 24],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 8,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 22, 24],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 9,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [33, 31, 28, 25, 24],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 10,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 38.3, 38.2],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 11,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 38.3, 38.2, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
-           }
-       },
-       {
-           id: 12,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [92, 90, 92, 91, 92]
-           }
-       },
-       {
-           id: 13,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [98, 96, 97, 94, 92]
-           }
-       },
-       {
-           id: 14,
-           data: {
-               "Heart rate (beats/min)": [73, 68, 72, 74, 75],
-               "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
-               "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
-               "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
-               "Oxygen saturation (%, breathing room air)": [84, 85, 89, 90, 92]
-           }
-       }
-   ];
+    
+const scenarios = [
+  {
+    id: 1,
+    data: {
+      "Heart rate (beats/min)": [112, 105, 113, 109, 110],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 2,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 85, 92, 110],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 3,
+    data: {
+      // Updated heart rate values
+      "Heart rate (beats/min)": [112, 105, 131, 135, 110],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 4,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["97/63", "93/66", "99/57", "96/60", "95/55"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 5,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "128/71", "103/60", "95/55"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 6,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      // Updated blood pressure values
+      "Blood pressure (mmHg)": ["97/63", "93/66", "82/47", "78/42", "95/55"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 7,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [24, 23, 23, 25, 24],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 8,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 22, 24],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 9,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      // Updated respiratory rate values
+      "Respiratory rate (breaths/min)": [24, 23, 33, 31, 24],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 10,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 38.3, 38.2],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 11,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 38.3, 38.2, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 98, 98]
+    }
+  },
+  {
+    id: 12,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [92, 90, 92, 91, 92]
+    }
+  },
+  {
+    id: 13,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      "Oxygen saturation (%, breathing room air)": [98, 96, 97, 94, 92]
+    }
+  },
+  {
+    id: 14,
+    data: {
+      "Heart rate (beats/min)": [73, 68, 72, 74, 75],
+      "Blood pressure (mmHg)": ["132/75", "140/78", "131/66", "135/73", "130/70"],
+      "Respiratory rate (breaths/min)": [18, 18, 19, 19, 18],
+      "Temperature (℃)": [36.8, 36.6, 36.6, 36.5, 36.8],
+      // Updated oxygen saturation values
+      "Oxygen saturation (%, breathing room air)": [92, 90, 84, 85, 92]
+    }
+  }
+];
+
    const timePoints = [
     "72 hours previously",
     "24 hours previously",
