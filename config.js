1df60a7d9dd76a432344925b2932f866efce51b7
Troy Kettle <89986560+Troy-Kettle@users.noreply.github.com>
2025-04-10 14:14:41 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDUqolrnw7CQ-_bPWx0bbSPBEK4MNgkgmo",
+    authDomain: "datacollection-9f8f6.firebaseapp.com",
+    projectId: "datacollection-9f8f6",
+    storageBucket: "datacollection-9f8f6.appspot.com",
+    messagingSenderId: "171849859334",
+    appId: "1:171849859334:web:c0c40fd8ca74a9e10e9e87",
+    measurementId: "G-F2WDNKDP5R"
+};
+
+// Export the configuration
+function getFirebaseConfig() {
+    return firebaseConfig;
+}
